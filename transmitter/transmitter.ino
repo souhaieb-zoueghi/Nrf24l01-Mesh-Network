d0e75d5c3a94cd4def91c99d16b494fd83aac2fc
souhaieb zoueghi <sou.zoueghi@gmail.com>
2020-03-20 19:57:42 +0100

    transmitter commented

--- transmitter/transmitter.ino
+++ transmitter/transmitter.ino
@@ -1,159 +1,115 @@
 #include <nRF24L01.h>
-//#include <printf.h>
 #include <RF24.h>
 #include <SPI.h>
 #include <RF24_config.h>
 #include <MQ2.h>
 #include "dht.h"
 
-#define dht_apin A1
+#define DHT_PIN A1
+#define MQ2_PIN A0
 
-#define DEBUG 1
+//nRF24L01 specifications
+#define CE_PIN   9
+#define CSN_PIN 10
+#define RETRIES_DELAY 15   //maximum
+#define RETRIES_NUMBER 15  //maximum
+#define FAILURE_THRESHOLD 4
 
-RF24 radio(9, 10);
+#define TRANSMITTER_ID 1
+#define TRANSCEIVER_ID 2
+#define TRANSCEIVER_NOT_DOWN 0
+#define DATA_SIZE 6
+#define DEBUG 0
 
-const byte addresses[][6] = {"1Node","2Node","3Node"};
+RF24 radio(CE_PIN, CSN_PIN);
 
-float data[5];
+const byte pipes[][6] = {"1Node","2Node"};
 
-int windSpeed = 0, sensorValue;
+float data_node1[DATA_SIZE];
 
-MQ2 mq2(A0);
+MQ2 mq2(MQ2_PIN);
 
-int ack = 0;
+dht DHT;
 
 int count = 0;
 
 void setup() {
-  
-  // put your setup code here, to run once:
-Serial.begin(9600);
-mq2.begin();
-radio.begin();
-delay(100);
+
+  Serial.begin(9600);
+
+  mq2.begin();
+  radio.begin();
+
+  delay(100);
+
   radio.setAutoAck(true);
   radio.enableAckPayload();
   radio.enableDynamicPayloads();
   radio.stopListening();
-  radio.openWritingPipe(addresses[0]);
-  radio.setRetries(15,15);
+  radio.openWritingPipe(pipes[0]);
+  radio.setRetries(RETRIES_DELAY,RETRIES_NUMBER);
+
 }
-
-float co, smoke, hum, temp;
-
-//const char text[] = "souhaib";
-dht DHT;
 
 void loop() {
 
-  co = mq2.readCO();
-  smoke = mq2.readSmoke();
+  float co = mq2.readCO();
+  float smoke = mq2.readSmoke();
   delay(500);
-  
-  DHT.read11(dht_apin);
+
+  DHT.read11(DHT_PIN);
   delay(1000);
-  hum = DHT.humidity;
-  temp = DHT.temperature;
-
- /*sensorValue = analogRead(A3);
- sensorValue = sensorValue * (5.0 / 1023.0);
- delay(500);*/
-
-  //windSpeed = getWindSpeed();
+  float humidity = DHT.humidity;
+  float temperature = DHT.temperature;
 
 #if DEBUG == 1
- Serial.println(sensorValue);
-  Serial.println(windSpeed);
-  #endif
 
-  
-Serial.println(co);
-Serial.println(smoke);
-Serial.println(hum);
-Serial.println(temp);
+  Serial.println(co);
+  Serial.println(smoke);
+  Serial.println(humidity);
+  Serial.println(temperature);
 
-  data[0] = 1;
-  data[1] = co;
-  data[2] = smoke;
-  data[3] = hum;
-  data[4] = temp;
-  //data[5] = windSpeed;
-  
-  
-  radio.write(&data, sizeof(data),0);
-  
-//delay(1000);
+#endif
 
-//radio.startListening();
+  data_node1[0] = TRANSMITTER_ID;   //Node id
+  data_node1[1] = co;
+  data_node1[2] = smoke;
+  data_node1[3] = hum;
+  data_node1[4] = temp;
+  data_node1[5] = TRANSCEIVER_NOT_DOWN;
 
-if(radio.isAckPayloadAvailable()){
-  
-  radio.read(&ack,sizeof(int));
-  Serial.println("received ack payload is : ");
-  Serial.println(ack);
-  count = 0;
-  
-}
+  if(radio.write(&data_node1, sizeof(data_node1))){
 
-else{
-  Serial.println("Failure !!!");
-  count++;
-}
+    Serial.println("Ack payload received successfully");
+    count = 0;
 
-if(count >= 10){
-  
-  Serial.println("Node 2 is down");
-  radio.stopListening();
-  delay(200);
-  radio.openWritingPipe(addresses[1]);
-  data[0] = 3;
+  }else{
 
-  radio.write(&data, sizeof(data));
+    count++;
 
+  }
 
-  delay(200);
-  radio.stopListening();
-  delay(200);
-  radio.openWritingPipe(addresses[0]);
+  if(count > FAILURE_THRESHOLD){
 
+    //the transceiver is down
+    Serial.println("Failure !!!");
+    Serial.println("the transceiver is down");
+
+    //we must switch the writing pipe to send data directly to the gateway
+    radio.stopListening();
+    delay(200);
+    radio.openWritingPipe(pipes[1]);
+    //we put the id of the transceiver to inform the gateway that its down
+    data_node1[5] = TRANSCEIVER_ID;
+
+    radio.write(&data_node1, sizeof(data_node1));
+
+    delay(200);
+    //we switch the writing pipe to try to reach the transceiver every time
+    radio.stopListening();
+    delay(200);
+    radio.openWritingPipe(pipes[0]);
 
 }
 
-
-
-
-
-/*
-
-unsigned long started_waiting_at = micros();               // Set up a timeout period, get the current microseconds
-    boolean timeout = false;                                   // Set up a variable to indicate if a response was received or not
-    
-    while ( ! radio.available() ){                             // While nothing is received
-      if (micros() - started_waiting_at > 500000 ){            // If waited longer than 200ms, indicate timeout and exit while loop
-          timeout = true;
-          break;
-      }      
-    }
-
-    if ( timeout ){                                             // Describe the results
-        Serial.println("Failed, response timed out.");
-    }else{
-        Serial.println("Packet sent successfully");
-}*/
 }
-/*
-int getWindSpeed(){
-if((sensorValue >= 0) && (sensorValue < 10))
-  return 0;
-  
-if((sensorValue) >= 10 && (sensorValue <= 30))
-  return 10;
-   
-if((sensorValue) > 30 && (sensorValue <= 70))
-  return 20;
-
-if((sensorValue > 70) && (sensorValue <= 100))
-  return 30;
-
-return 40; 
-}*/
